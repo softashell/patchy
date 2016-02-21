1f09c13d5e729581a3a80f609eee78178651253b
soft as HELL <softashell@cock.li>
2016-02-21 19:03:01 +0200

    Overwrite files with ffmpeg when transcoding

--- src/transcoder.go
+++ src/transcoder.go
@@ -8,9 +8,9 @@ import (
 
 func transcode(song string) {
 	cmd := "ffmpeg"
-	args := []string{"-i", song, "-threads", "0", "-acodec", "libopus", "-b:a", "128k", "-vbr", "on", "static/queue/next.opus"}
+	args := []string{"-i", song, "-y", "-threads", "0", "-acodec", "libopus", "-b:a", "128k", "-vbr", "on", "static/queue/next.opus"}
 	if err := exec.Command(cmd, args...).Run(); err != nil {
-		fmt.Fprintln(os.Stderr, "Fatal Error, could not transcode song! Additional info: "+err.Error())
+		fmt.Fprintln(os.Stderr, "Error, could not transcode song! Additional info: "+err.Error())
 		os.Exit(1)
 	}
 }
